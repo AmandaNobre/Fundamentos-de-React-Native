2f32ce2b12333570c75c246dce9450a2d0f699d4
AmandaNobre <amandanobree71@gmail.com>
2020-08-12 10:45:55 -0300

    Sistema de modulos do ECMAScript

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { View } from 'react-native'
 
 import Primeiro from './components/Primeiro'
+import {Comp1, Comp2, Comp3} from './components/Mult'
 
 export default () => (
     <View>
+        <Comp1/>
+        <Comp2/>
+        <Comp3/>
         <Primeiro />
     </View>
 )
